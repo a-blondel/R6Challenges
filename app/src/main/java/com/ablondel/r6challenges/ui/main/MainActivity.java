ca41b3fcfb4eedab264dc873d16df27572c51fcd
a-blondel <ablondel@protonmail.com>
2022-04-17 02:03:15 +0200

    Starting LoginActivity when the logout button is pressed

--- app/src/main/java/com/ablondel/r6challenges/ui/main/MainActivity.java
+++ app/src/main/java/com/ablondel/r6challenges/ui/main/MainActivity.java
@@ -2,9 +2,13 @@ package com.ablondel.r6challenges.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.ablondel.r6challenges.R;
+import com.ablondel.r6challenges.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton logoutButton = findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
